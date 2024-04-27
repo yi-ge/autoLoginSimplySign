50672b8b99d3fc84b946093009fc328b39d5918b
yi-ge <a@wyr.me>
2024-04-27 15:50:33 +0800

    test: 测试稳定性

--- task.py
+++ task.py
@@ -1,15 +1,24 @@
-from time import sleep
 import pyautogui
+
+pyautogui.PAUSE = 2.5  # PyAutoGUI函数增加延迟为2.5秒：
 
 
 def task():
     print("执行定时任务")
-    button_x, button_y = pyautogui.locateCenterOnScreen('1.png')
-    if button_x is not None:
-        pyautogui.click(button='right', x=button_x, y=button_y)
-        sleep(1)
-        button_x, button_y = pyautogui.locateCenterOnScreen('2.png')
-        pyautogui.click(button_x, button_y)
+    # try:
+    button_x, button_y = pyautogui.locateCenterOnScreen(
+        '1.png', confidence=0.9, grayscale=True)
+    pyautogui.click(button='right', x=button_x, y=button_y)
+    # im1 = pyautogui.screenshot()
+    # im1.save('my_screenshot.png')
+    buttonLocation = pyautogui.locateOnScreen(
+        '2.png', confidence=0.9, grayscale=True)
+    pyautogui.click(buttonLocation[0] + 3, buttonLocation[1] + 3)
+
+    pyautogui.write('Hello world!', interval=0.25)
+    pyautogui.keyDown('enter')
+    # except Exception as e:
+    #     print(e)
 
 
 task()
